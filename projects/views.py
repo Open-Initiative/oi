2184b660c9da1372ef2de1418c7ee034db795cc9
Maxi <m.mampuya@laposte.net>
2012-05-25 11:54:33 +0100

    bugfix: invited user follow the project

--- projects/views.py
+++ projects/views.py
@@ -510,7 +510,7 @@ def shareproject(request, id):
     except (KeyError, User.DoesNotExist):
         return HttpResponse(_("Cannot find user"), status=531)
     project.apply_perm(user, OI_ALL_PERMS)
-    request.user.get_profile().follow_project(project)
+    user.get_profile().follow_project(project)
     messages.info(request, _("Task shared"))
     return HttpResponse('', status=332)
 
