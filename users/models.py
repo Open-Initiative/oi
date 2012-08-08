a8cab899a11b3a2be7bb27fb1a246b1701fbad7a
Maxi <m.mampuya@laposte.net>
2012-08-08 09:58:04 +0100

    remove observe project remove observe project in data base

--- users/models.py
+++ users/models.py
@@ -37,7 +37,6 @@ class UserProfile(models.Model):
     blog = models.ForeignKey(Message, unique=True)
     rss_feed = models.URLField(verify_exists=False, blank=True)
     last_feed = models.DateTimeField(null=True, blank=True)
-    observed_projects = models.ManyToManyField(Project, related_name="followers", blank=True)
     personal_website = models.URLField(max_length=200, blank=True, null=True)
     
     def get_titles(self):
