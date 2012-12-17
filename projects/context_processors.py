8f6ecf9e151b1f38a63a70c3acc61f0680142ce2
Sylvain <sylvain.lebon@openinitiative.com>
2012-12-17 20:35:50 +0100

    ui: new project page syntax: refactoring

--- projects/context_processors.py
+++ projects/context_processors.py
@@ -1,6 +1,6 @@
-from oi.helpers import OI_PRJ_STATES, OI_COMMISSION
+from oi.helpers import OI_PRJ_STATES, OI_PRJ_PHASES, OI_COMMISSION
 
 def constants(request):
     """return specific constants for templates as a dictionnary."""
-    return {'OI_COMMISSION': OI_COMMISSION, 'OI_PRJ_STATES': OI_PRJ_STATES}
+    return {'OI_COMMISSION': OI_COMMISSION, 'OI_PRJ_STATES': OI_PRJ_STATES, 'OI_PRJ_PHASES': OI_PRJ_PHASES}
 
