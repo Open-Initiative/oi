a85ffac68cbccfd4d74ab0307c622f6e5b8a633b
Sylvain Le Bon <lebon.sylvain@gmail.com>
2012-12-29 13:23:12 +0100

    bugfix: site domain is specific

--- messages/management.py
+++ messages/management.py
@@ -3,13 +3,14 @@ from django.conf import settings
 from django.db.models import signals
 from django.utils.translation import ugettext_noop as _
 from django.contrib.sites.models import Site
+from oi.settings import OI_DOMAIN
 
 def register_site(app, created_models, verbosity, **kwargs):
     if Site.objects.count() > 0:
         site = Site.objects.get_current()
     else:
         site = Site()
-    site.domain = u"www.open-initiative.com"
+    site.domain = OI_DOMAIN
     site.name = u"Open Initiative"
     site.save()
     
