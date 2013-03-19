b02384400fca66d05786814179e65f5d57c51bae
Sylvain <sylvain.lebon@openinitiative.com>
2013-03-19 19:16:11 +0100

    ui: links on home page

--- projects/context_processors.py
+++ projects/context_processors.py
@@ -4,5 +4,5 @@ from oi.settings import github_id
 
 def constants(request):
     """return specific constants for templates as a dictionnary."""
-    return {'Sites': Site.objects, 'current_site': Site.objects.get_current(), 'github_id': github_id, 'OI_COMMISSION': OI_COMMISSION, 'OI_PRJ_STATES': OI_PRJ_STATES, 'OI_PRJ_PHASES': OI_PRJ_PHASES}
+    return {'Sites': dict(map(lambda l:map(str, l), Site.objects.values_list("name", "domain"))), 'current_site': Site.objects.get_current(), 'github_id': github_id, 'OI_COMMISSION': OI_COMMISSION, 'OI_PRJ_STATES': OI_PRJ_STATES, 'OI_PRJ_PHASES': OI_PRJ_PHASES}
 
