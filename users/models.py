643d6d576b8a999338423052df4e9eb1fc9578aa
Maxi <m.mampuya@laposte.net>
2013-10-03 12:01:22 +0200

    update: edit date manually

--- users/models.py
+++ users/models.py
@@ -213,7 +213,7 @@ class Payment(models.Model):
 class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('address','postcode','city','country','mobile','phone','personal_website')
+        fields = ('birthdate','address','postcode','city','country','mobile','phone','personal_website')
 
 class SkillForm(ModelForm):
     class Meta:
