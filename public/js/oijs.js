e7a1b0ae0a21099e6bfee9a49d4d7124ee689cb0
Maxi <m.mampuya@laposte.net>
2013-10-25 10:17:13 +0200

    update: less toolbar in the tinymce

--- public/js/oijs.js
+++ public/js/oijs.js
@@ -252,24 +252,27 @@ function setPrjState(span,statenum) {
 }
 
 //I created a object to init the tinymce in textarea and call it instead of mceAddControl in other js function
-objectInitTinyMce = {selector: "textarea",
+objectInitTinyMce = {
+    selector: "textarea",
     theme: "modern",
     width: 500,
     height: 200,
+    menu: true,
     plugins: [
          "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
          "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
          "save table contextmenu directionality emoticons template paste textcolor"
    ],
    content_css: "/css/tinymce.css",
-   toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons", 
+   toolbar: "insertfile | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | preview media fullpage | forecolor backcolor emoticons", 
    style_formats: [
         {title: 'Bold text', inline: 'b'},
         {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
         {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
         {title: 'Table styles'},
         {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
-    ]}
+    ]
+}
     
 //init tinymce in the web page after loading
 tinyMCE.init(objectInitTinyMce); 
