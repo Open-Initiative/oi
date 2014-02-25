37b171c006feedd73ea97a3e157aea31a51d6bfb
Maxi <m.mampuya@laposte.net>
2014-02-25 14:51:22 +0100

    update: no zoom bugfix: can close popup

--- public/js/oijs.js
+++ public/js/oijs.js
@@ -93,6 +93,8 @@ function hidePopups() {
 function addPopup(popup) {
     document.popups = (document.popups || []).concat(popup);
     addEvent(document, "click", hidePopups);
+//    soit addEvent ou addEventListener mais pas les deux
+    document.getElementById("blurDiv").addEventListener('click', hidePopups , false);
 }
 function showPopup(popup) {
     if(!document.getElementById("blurDiv")){
