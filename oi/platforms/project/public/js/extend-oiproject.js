f9d1a4981ea9275d8f6ca38b1f2e31d1001a2229
maxi <m.mampuya@laposte.net>
2014-09-12 19:31:40 +0200

    update: change files folder

---
+++ oi/platforms/project/public/js/extend-oiproject.js
@@ -0,0 +1,61 @@
+function addSpec(projectid) {
+    var divid = newDiv("specs_"+projectid);
+    OIajaxCall(prjsite+"/prjmgt/"+projectid+"/editspec/0?divid="+divid+"&specorder=-1", null, divid, 
+        function(){changeSpecType(divid, 1);});
+        document.getElementById(divid).scrollIntoView();
+}
+function editSpec(projectid, specorder, type) {
+    var specid = getValue("specid_"+projectid+"_"+specorder);
+    var divid = "spec_"+projectid+"_"+specorder;
+    OIajaxCall(prjsite+"/prjmgt/"+projectid+"/editspec/"+specid+"?divid="+divid, null, divid,
+        function(){changeSpecType(divid, type);});
+}
+function changeSpecType(divid, type) {
+    if(getValue("type_"+divid)==1)tinymce.remove('#text_'+divid);
+    var text = getValue("text_"+divid);
+    var projectid = getValue("projectid_"+divid);
+    var specid = getValue("specid_"+divid);
+    document.getElementById("type"+getValue("type_"+divid)+"_"+divid).className = "spectype";
+    document.getElementById("type"+type+"_"+divid).className = "spectype spectypeselected";
+    document.getElementById("type_"+divid).value = type;
+    var url = prjsite+"/prjmgt/"+projectid+"/editspecdetails/"+specid+"?divid="+divid+"&type="+type+"&ts="+(new Date()).getTime();
+    OIajaxCall(url, null, "spec_"+divid, 
+        function(){
+            if(getValue("type_"+divid)==6) buildText(divid);
+            if(getValue("type_"+divid)==1){
+                var ed = new tinymce.Editor('text_'+divid, objectInitTinyMce, tinymce.EditorManager);
+                ed.render();
+            }
+        });
+}
+function validatorProject(projectid){
+    OIajaxCall("/prjmgt/validator/"+projectid, null, "prjdialogue_"+projectid,
+        function(){show("prjdialogue_"+projectid);
+        document.getElementById('validator_'+projectid).focus();});
+}
+function shareProject(projectid) {
+    OIajaxCall("/prjmgt/"+projectid+"/share", null, "prjdialogue_"+projectid, 
+        function(){show("prjdialogue_"+projectid);
+        document.getElementById('usershare_'+projectid).focus();
+        });
+}
+function evalProject(projectid) {
+    OIajaxCall("/prjmgt/eval/"+projectid, null, "prjdialogue_"+projectid, 
+        function(){show("prjdialogue_"+projectid);});
+}
+function bidProject(projectid) {
+    OIajaxCall("/prjmgt/bid/"+projectid, null, "prjdialogue_"+projectid,
+        function(){show("prjdialogue_"+projectid);
+            document.getElementById('bid_'+projectid).focus();
+        }
+    );
+}
+function delegateProject(projectid) {
+    OIajaxCall("/prjmgt/delegate/"+projectid, null, "prjdialogue_"+projectid, 
+        function(){show("prjdialogue_"+projectid);document.getElementById('delegate_to_'+projectid).focus();});
+}
+function offerProject(projectid) {
+    OIajaxCall("/prjmgt/offer/"+projectid, null, "prjdialogue_"+projectid, 
+        function(){show("prjdialogue_"+projectid);
+        document.getElementById('offer_'+projectid).focus();});
+}
