db9119c02efbdf65c6b6172e010f24649faeb5e7
maxi <m.mampuya@laposte.net>
2015-03-19 16:08:00 +0100

    bugfix: refuse to put money on open initiative redirecte me on open initiative

--- oi/projects/context_processors.py
+++ oi/projects/context_processors.py
@@ -5,5 +5,5 @@ from oi.helpers import OI_PRJ_STATES, OI_PRJ_PHASES, OI_COMMISSION, SPEC_TYPES
 
 def constants(request):
     """return specific constants for templates as a dictionnary."""
-    return {'Sites': dict(map(lambda l:map(str, l), Site.objects.values_list("name", "domain"))), 'current_site': Site.objects.get_current(), 'github_id': OI_GITHUB_ID, 'OI_COMMISSION': OI_COMMISSION, 'OI_PRJ_STATES': OI_PRJ_STATES, 'OI_PRJ_PHASES': OI_PRJ_PHASES, 'REDIRECT_URL':settings.REDIRECT_URL, 'types': SPEC_TYPES}
+    return {'Sites': dict(map(lambda l:map(str, l), Site.objects.values_list("name", "domain"))), 'current_site': Site.objects.get_current(), 'github_id': OI_GITHUB_ID, 'OI_COMMISSION': OI_COMMISSION, 'OI_PRJ_STATES': OI_PRJ_STATES, 'OI_PRJ_PHASES': OI_PRJ_PHASES, 'REDIRECT_URL':settings.REDIRECT_URL, 'types': SPEC_TYPES, 'site_name':settings.SITE_NAME}
 
