55b9ea9279321a95080a74d7f358bc3f6ddef7bc
maxi <m.mampuya@laposte.net>
2015-03-20 11:56:52 +0100

    update: refactoring

--- oi/platforms/project/public/js/oiproject.js
+++ oi/platforms/project/public/js/oiproject.js
@@ -151,8 +151,7 @@ function setActiveTask(projectid, canAdd) {
         form.style.margin = "5px 0";
         form.style.width = "220px";
         form.innerHTML = '<input type="image" src="/img/icons/addtask.png" alt="'+gettext("New task")+'" title="'+gettext("New task")+'" />'+
-            '<input type="text" id="newtask_title_'+projectid+'" class="newtask_title" value="'+gettext("New task")+'" '+
-            'onclick="if(this.value==\''+gettext("New task")+'\')this.value=\'\'" onblur="if(!this.value)this.value=\''+gettext("New task")+'\'"/>';
+            '<input type="text" id="newtask_title_'+projectid+'" class="newtask_title" placeholder="'+gettext("New task")+'"/>';
         oiTree.selected = projectid;
         if(oiTable) oiTable.addSpace(projectid);
     }
