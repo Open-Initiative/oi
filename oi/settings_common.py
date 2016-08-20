09e3d5e6907b0a6758e5905d463039f485dc1938
Sylvain <sylvain.lebon@openinitiative.com>
2016-08-20 15:34:15 +0200

    bugfix: missing CORS headers on OPTIONS requests

--- oi/settings_common.py
+++ oi/settings_common.py
@@ -128,7 +128,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ) #+ SPECIFIC_MIDDLEWARE
 
 TEMPLATE_CONTEXT_PROCESSORS = (
